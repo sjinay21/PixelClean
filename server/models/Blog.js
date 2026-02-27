daf8b4f59a40bc3e299e57336ad03f5dbd6f240f
Jinay Shah <166685469+sjinay21@users.noreply.github.com>
2026-02-27 13:21:27 +0530

    Add files via upload

---
+++ server/models/Blog.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      default: "Admin",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
