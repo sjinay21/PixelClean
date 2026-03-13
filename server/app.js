8fec3e8b68c388938deee61f088e1997cf1e2c9a
Jinay Shah <166685469+sjinay21@users.noreply.github.com>
2026-03-13 22:13:42 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const cors = require("cors");
 const path = require("path");
 const authRoutes = require("./core/auth/auth.routes");
 const authMiddleware = require("./middleware/auth.middleware");
 const watermarkRoutes = require("./modules/watermark/watermark.routes");
+const enhanceRoutes = require("./modules/enhance/enhance.routes");
+const pdfRoutes = require("./modules/pdf/pdf.routes");
+const backgroundRoutes = require("./modules/background/background.routes");
+const videoRoutes = require("./modules/video/video.routes");
+const logoRoutes = require("./modules/logo/logo.routes");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +16,11 @@ app.get("/", (req, res) => {
 });
 app.use("/api/auth", authRoutes);
 app.use("/api/watermark", watermarkRoutes);
+app.use("/api/pdf", pdfRoutes);
+app.use("/api/enhance", enhanceRoutes);
+app.use("/api/background", backgroundRoutes);
+app.use("/api/video", videoRoutes);
+app.use("/api/logo", logoRoutes);
 app.get("/api/protected", authMiddleware, (req, res) => {
   res.json({
     message: "Protected route accessed",
@@ -21,5 +29,3 @@ app.get("/api/protected", authMiddleware, (req, res) => {
 });
 
 module.exports = app;
-
-
